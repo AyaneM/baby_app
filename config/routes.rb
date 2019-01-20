4b865afdacdb999083637550d83649d100bffb2e
ayane.mitsuishi <ayane.mitsuishi@techacademy.jp>
2019-01-20 06:07:19 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'home_pages#home'
   get '/about', to: 'home_pages#about'
   get '/help', to: 'home_pages#help'
   get  '/contact', to: 'home_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
+  
 end
